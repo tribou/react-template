8ca25ddd57bbc57223c743c7d6e17ee7961d8f0a
Aaron Tribou <aaron.tribou@gmail.com>
2017-07-09 22:18:45 -0500

    Add bump_version circleci script

---
+++ bin/bump_version.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Don't bump version if we already have a tag at HEAD
+(git describe --exact-match --tags HEAD) > /dev/null 2>&1
+if [ "$?" -eq "0" ]
+then
+  exit
+fi
+
+CURRENT=$(cat package.json | grep '"version"' | awk -F '"' '{print $4}')
+PREMAJOR=$(echo "$CURRENT" | grep '\.0\.0-0')
+PREMINOR=$(echo "$CURRENT" | grep -v '\.0\.0-0' | grep '\.0-0')
+
+if [ -n "$PREMAJOR" ]
+then
+
+  echo "PREMAJOR $PREMAJOR"
+  npm version major
+
+elif [ -n "$PREMINOR" ]
+then
+
+  echo "PREMINOR $PREMINOR"
+  npm version minor
+
+else
+
+  echo "PATCH $CURRENT"
+  npm version patch
+
+fi
+
+git push origin master --follow-tags
