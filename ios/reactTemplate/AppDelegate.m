dae1e8a8b807ff691dd1e9dac990b78c3cb39f47
Aaron Tribou <aaron.tribou@gmail.com>
2017-11-11 10:55:37 -0600

    Add Firebase dep to AppDelegate Add React dep to project build phase

--- ios/reactTemplate/AppDelegate.m
+++ ios/reactTemplate/AppDelegate.m
@@ -12,6 +12,8 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+@import Firebase;
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -31,6 +33,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [FIRApp configure];
   return YES;
 }
 
