2d9bc0866519630819d2f3b00419e269a57b6551
Aaron Tribou <aaron.tribou@gmail.com>
2018-04-02 07:58:21 -0500

    Fix post-prettier flow errors

--- config/webpack/blocks/getTarget.js
+++ config/webpack/blocks/getTarget.js
@@ -1,21 +1,19 @@
 // @flow
 
 function getTarget(target /* : ?string */) {
-	return ({ platform }) =>
-		/* : { platform: Platform } */
-		{
-			if (target) return { target }
+	return ({ platform } /* : { platform: Platform } */) => {
+		if (target) return { target }
 
-			if (platform === 'browser') {
-				return { target: 'web' }
-			} else if (platform === 'desktop') {
-				return { target: 'electron-renderer' }
-			} else if (platform === 'server') {
-				return { target: 'node' }
-			}
-
-			throw new Error('Unsupported platform and no target passed')
+		if (platform === 'browser') {
+			return { target: 'web' }
+		} else if (platform === 'desktop') {
+			return { target: 'electron-renderer' }
+		} else if (platform === 'server') {
+			return { target: 'node' }
 		}
+
+		throw new Error('Unsupported platform and no target passed')
+	}
 }
 
 module.exports = getTarget
